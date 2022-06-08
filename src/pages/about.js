f8c47d0213e0dfe40f4c1ad6ee54d784448f12e1
Julia Rajmann <juliarajmann@gmail.com>
2022-06-08 22:37:12 +0200

    Add styles and layout

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+// import { Link } from "gatsby";
+import Layout from "./layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="My wonderful cats">
+      <p>Hello! Meet my purrrfect cats :3</p>
+      {/* <Link to="/">Back to Home</Link> */}
+    </Layout>
+  );
+};
+
+export default AboutPage;
